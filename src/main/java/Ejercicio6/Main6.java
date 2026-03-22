b26b41670324c45364938872bbfde6e6674e1c5b
Javiep <javi.escalona@duocuc.cl>
2026-03-22 17:51:15 -0300

    Avance ejercicio 6 y 7

--- src/main/java/Ejercicio6/Main6.java
+++ src/main/java/Ejercicio6/Main6.java
@@ -1,26 +1,48 @@
-import java.util.ArrayList;
+package Ejercicio6;
 
-void main(String[] args) {
-    int contador = 1;
-    String aBuscar = "Huevos";
-    ArrayList<String> carrito = new ArrayList<>();
-    carrito.add("Leche");
-    carrito.add("Pan");
-    carrito.add("Huevos");
-    carrito.add("Mantequilla");
-    carrito.add("Jugo");
-    System.out.println("=== Carrito de compras ===");
-    for (String i : carrito){
-        System.out.printf("%d. %s\n",contador++,i);
-    }
-    System.out.printf("Total de productos: %d",contador-1);
-    System.out.println("");
-    for (String i : carrito){
-        if (i.equals(aBuscar)){
-            System.out.println(aBuscar+" está en el carrito.");
-            return;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args) {
+        ArrayList<String> carrito = new ArrayList<>();
+        boolean salir = true;
+
+        while (salir) {
+            int option = menu();
+            switch (option) {
+                case 1:
+                    String producto = agregarProducto();
+                    carrito.add(producto);
+                    System.out.println("Has agregado: " + producto);
+                    break;
+                case 6:
+                    System.out.println("\nHas salido del programa...");
+                    salir = false;
+            }
         }
-    }System.out.println(aBuscar+" no está en el carrito.");
+    }
+
+public static int menu(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("""
+            1. Agregar productos
+            2. Mostrar carrito
+            3. Buscar productos
+            4. Eliminar productos
+            5. Mostrar carrito
+            6. Salir
+            """);
+    System.out.println("\nElija una opción...");
+    int opt = sc.nextInt();
+    return opt;
 }
 
+public static String agregarProducto(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Producto a agregar: ");
+    String productoAgregar = sc.nextLine();
+    return productoAgregar;
+}
+}
 
--- src/main/java/Ejercicio7/Main7.java
+++ src/main/java/Ejercicio7/Main7.java
@@ -1,3 +1,59 @@
-void main(String[] args) {
+package Ejercicio7;
+
+import java.util.HashMap;
+
+public class Main7 {
+    public static void main(String[] args) {
+        HashMap<String, String> agenda = new HashMap<>();
+        agenda.put("Ana López", "+56 9 1234 5678");
+        agenda.put("Carlos Ruiz", "+56 9 8765 4321");
+        agenda.put("María González", "+56 9 5555 0000");
+        agenda.put("Pedro Soto","+56 9 1111 2222");
+
+        mostrarContacto(agenda);
+
+        buscarContacto(agenda);
+
+        actualizarContacto(agenda);
+
+        eliminarContacto(agenda);
+
+        mostrarContacto(agenda);
+
+    }
+    public static void mostrarContacto(HashMap agenda){
+        System.out.println("=== Agenda de Contactos ===");
+        for (Object i : agenda.keySet()){
+            System.out.println(i+" -> "+agenda.get(i));
+        }
+        System.out.println("Total contactos: "+agenda.size());
+    }
+    public static void buscarContacto(HashMap agenda) {
+        String aBuscar = "Carlos Ruiz";
+
+        if (agenda.containsKey(aBuscar)){
+            System.out.println("Contacto encontrado -> "+aBuscar+": "+agenda.get(aBuscar));
+        } else {
+            System.out.println("Contacto no encontrado -> "+aBuscar);
+        }
+    }
+    public static void actualizarContacto(HashMap agenda){
+        String aActualizar = "Ana López";
+        if (agenda.containsKey(aActualizar)){
+            agenda.put(aActualizar, "+56 9 9999 8888");
+            System.out.println("Contacto actualizado -> "+aActualizar+": "+agenda.get(aActualizar));
+        } else {
+            System.out.println("Contacto no encontrado -> "+aActualizar);
+        }
+    }
+    public static void eliminarContacto(HashMap agenda){
+        String aEliminar = "Pedro Soto";
+        if (agenda.containsKey(aEliminar)){
+            agenda.remove(aEliminar);
+            System.out.println("Contacto eliminado -> "+aEliminar);
+        } else {
+            System.out.println("Contacto no encontrado -> "+aEliminar);
+        }
+    }
 
 }
